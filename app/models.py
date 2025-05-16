07e9e0c92f68553b54fb1742c8eaaf327cbef5e7
Igor Chernenko <166181364+Igor-Chernenko@users.noreply.github.com>
2025-05-16 15:45:22 -0700

    initial completion of tables: adventures, comments,images, likes through alembic

--- app/models.py
+++ app/models.py
@@ -5,10 +5,11 @@ Database.py
 
 handles SQLalchemy Database Model Schema creation (Database strucuture)
 
-Model Design Verion: 0.1
+Model Design Verion: 1.0
 """
 from app.database import Base
-from sqlalchemy import text, Column, Integer, String, TIMESTAMP
+from sqlalchemy import text, Column, Integer, String, Text, TIMESTAMP, ForeignKey
+from sqlalchemy.orm import relationship
 
 #----------------------------------[ Users ]----------------------------------
 """
@@ -30,4 +31,77 @@ class Users(Base):
 
 #----------------------------------[ Adventures ]----------------------------------
 
-    
+"""
+Data model for adventure table in Database
+    Collums:
+       adventure_id: int, primary_key
+       title: string
+       owner_id: int, 
+       created_at: time, 
+       Description: string, write about adventure
+
+    Relationship to Users:
+        Matches a user to the adventure based on the foreinKey Created
+"""
+
+class Adventures(Base):
+    __tablename__ = "adventures"
+    adventure_id = Column(Integer, nullable = False, primary_key = True)
+    title = Column(String, nullable= False)
+    owner_id = Column(Integer, ForeignKey("users.user_id", ondelete= "CASCADE"), nullable= False)
+    created_at = Column(TIMESTAMP(timezone= True),nullable= False, server_default= text("now()"))
+    description = Column(Text)
+
+    owner = relationship("Users")
+
+#----------------------------------[ Likes ]----------------------------------
+
+"""
+Data model for Likes table in Database
+    Collums:
+       post_id, Primary_key, foreign key is the adventure id so if an adventure is deleted so is this like
+       owner_id, Primary_key, foreign key is also the users id id so if a user is deleted so is this like
+
+"""
+
+class Likes(Base):
+    __tablename__ = "likes"
+    adventure_id = Column(Integer, ForeignKey("adventures.adventure_id", ondelete="CASCADE"), nullable = False, primary_key= True)
+    owner_id = Column(Integer, ForeignKey("users.user_id", ondelete="CASCADE"), nullable = False, primary_key= True)
+
+#----------------------------------[ Images ]----------------------------------
+
+"""
+Data model for images table in Database, This will contain urls to AWS S3 for storage
+    Collums:
+    image_id, Primary key
+    url, url to AWS S3
+    adventure_id, int, foreign key to adventure id
+    caption, str
+
+"""
+
+class Images(Base):
+    __tablename__ = "images"
+    image_id = Column(Integer, nullable=False, primary_key = True)
+    url = Column(String, nullable = False, unique=True)
+    adventure_id = Column(Integer, ForeignKey("adventures.adventure_id", ondelete="CASCADE"), nullable = False)
+    caption = Column(String)
+
+#----------------------------------[ Comments ]----------------------------------
+"""
+Data model for comments table in Database
+    Collums:
+    comment_id, int, primary key
+    owner_id, int, foreign key to users.user_id
+    created_at, time
+    adventure_id, int, foreign key to adventures.adventure_id
+    comment, string, main comment
+"""
+class Comments(Base):
+    __tablename__ = "comments"
+    comment_id = Column(Integer, primary_key=True, nullable = False)
+    owner_id = Column(Integer, ForeignKey("users.user_id", ondelete="CASCADE"), nullable = False)
+    adventure_id = Column(Integer, ForeignKey("adventures.adventure_id", ondelete="CASCADE"), nullable = False)
+    created_at = Column(TIMESTAMP(timezone=True),nullable = False, server_default=text("now()"))
+    comment = Column(String, nullable = False)
